52723353d3b8807e338d92ba67339582355865bf
Hiroshima <hirosh@wni.com>
2021-02-11 22:12:50 +0900

    [Add] Nest + Material-UI + StyledComponents

---
+++ front/components/Header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import * as React from 'react'
+
+export const Header : React.FC = ()=>{
+  return(
+    <header>
+      <nav>
+        <Link href='/'>
+
+        </Link>
+      </nav>
+    </header>
+  )
+}
